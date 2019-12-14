6cc1009fbe95fee50ab80c40711ef571f5b9aa6e
Blake Morgan <b.morgan.296@gmail.com>
2019-12-14 16:23:59 -0700

    Add other files.

---
+++ terraform/networks.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "172.20.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "172.20.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "172.20.2.0/24"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name   = "allow_ssh_22222"
+  vpc_id = aws_vpc.vpc.id
+
+  # TODO: Close down to IT VPN
+  ingress {
+    from_port    = 22222
+    to_port      = 22222
+    protocol     = "tcp"
+    cidr_blocker = ["0.0.0.0/0"]
+  }
+}
