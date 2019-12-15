e0f8db0572b267bf5a9e0666eb14928cc8a1e609
Blake Morgan <b.morgan.296@gmail.com>
2019-12-14 17:52:01 -0700

    Add compute resources

---
+++ terraform/compute.tf
@@ -0,0 +1,13 @@
+resource "aws_key_pair" "key" {
+  key_name   = "ec2-access-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "instance" {
+  ami                    = "ami-a0cfeed8"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key.key_name
+  monitoring             = true
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id              = aws_subnet.public.id
+}
